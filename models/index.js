817b003a8b71aaf1b1c08fed79dceee67c645ef9
Noah Becker <102703954+noah35becker@users.noreply.github.com>
2022-08-29 09:04:15 -0400

    fixed many-to-many aliases

--- models/index.js
+++ models/index.js
@@ -20,13 +20,13 @@ Category.hasMany(Product, {
 
 Product.belongsToMany(Tag, {
     through: ProductTag,
-    as: 'products', //confirm this later
+    as: 'tags',
     foreignKey: 'product_id'
 });
 
 Tag.belongsToMany(Product, {
     through: ProductTag,
-    as: 'tags', //confirm this later
+    as: 'products',
     foreignKey: 'tag_id'
 });
 
